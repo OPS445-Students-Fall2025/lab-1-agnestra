f917bcb4ab4c50d695e766b1a6a1b125cb86c3d6
Agnestra Mahat <amahat1@myseneca.ca>
2025-09-14 23:25:53 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#Thhe values for the variables name and age
+name = "Isaac"
+age =int(72)
+
+#this prints out the required output name is age yeard old!
+print(name + " is " + str(age) + " years old!")
+
