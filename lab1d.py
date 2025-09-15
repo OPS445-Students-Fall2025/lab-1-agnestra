410bf49f85bbdb95b00a0aaa79bdbcad8185e13e
Agnestra Mahat <amahat1@myseneca.ca>
2025-09-14 23:36:45 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#defines the variables x,y and z
+
+x = 10
+y = 2
+z = 5
+
+#this line does the maths and shows the process
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(x+y*z))
